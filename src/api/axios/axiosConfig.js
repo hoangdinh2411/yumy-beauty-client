733dedd730c267bd8909da9f88391df49c575512
hoangdinh2411 <hoangdinh2411@gmail.com>
2022-02-21 17:18:16 +0100

    update

--- src/api/axios/axiosConfig.js
+++ src/api/axios/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_BASE_URL ||'http://localhost:5000' ,
   timeout:10000,
   headers: {
     "content-type": "application/json",
